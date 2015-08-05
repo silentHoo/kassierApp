37cc3ed9e4d97702526a0f70dec1ef51ae38040c
Patrick Hillert <patrick.hillert@inovex.de>
2015-08-05 21:09:52 +0200

    Generate yeoman project app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kassierAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the kassierAppApp
+ */
+angular.module('kassierAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kassierAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kassierAppApp
+ */
+angular.module('kassierAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
